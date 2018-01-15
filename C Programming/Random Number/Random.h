e37ff289b7702e0c63731e264330b924348ea940
NanoCode012 <kevinvong@rocketmail.com>
2018-01-15 20:05:24 +0700

    Updated Random formula to be more 'random' even if it runs in the same millisecond.

--- C Programming/Random Number/Random.h
+++ C Programming/Random Number/Random.h
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-int GetRandomNumber(){
-    srand(time(NULL));
+int GetRandomNumber(int seed = 1){
+    if (seed == 0) seed = 1;
+    srand(seed * time(NULL));
     return rand();
 }
 
 int GetRandomNumber(int max){
-    return GetRandomNumber() % (max + 1);
+    return GetRandomNumber(max) % (max + 1);
 }
 
 int GetRandomNumber(int min, int max){
