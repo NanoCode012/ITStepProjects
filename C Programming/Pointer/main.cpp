ca965f40f2a09eb2684718eadb85496ab70eebc1
NanoCode012 <kevinvong@rocketmail.com>
2018-02-12 19:39:08 +0700

    Added more examples of pointer in Pointer

--- C Programming/Pointer/main.cpp
+++ C Programming/Pointer/main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+void OutputElement(int *num, int size){
+    for (int i = 0; i < size; i++)
+    {
+        cout << *(num + i) << " is at address " << (num + i) << endl;
+    }
+}
+
 int main(){
     const int size = 5;
     int nums[size] = {1,5,10,15,20};
@@ -11,5 +18,11 @@ int main(){
     {
         cout << *(p + i) << " is at address " << (p + i) << endl;
     }
+    delete p;//remove the pointer from RAM
+
+    cout << endl;
+
+    //Pass Array as pointer
+    OutputElement(nums, size);
     return 0;
 }
