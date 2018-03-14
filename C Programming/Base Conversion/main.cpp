0c0952a033cf80e55b1c9eab8d831bc2281fd8a7
NanoCode012 <kevinvong@rocketmail.com>
2018-03-14 22:49:53 +0700

    Added Base Conversion

---
+++ C Programming/Base Conversion/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+char possibleCharacters[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int number[100];
+
+int Pow(int num, int exponent)
+{
+    int result = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        result *= num;
+    }
+    return result;
+}
+
+void ConvertFromDecimal(int num, int base)
+{
+    if (base < 2 || base > 36) 
+    {
+        cout << "Base too low or too high! Base set to 10.";
+        base = 10;
+    }
+    int size = 0;
+    while(num > 0)
+    {
+        number[size++] = num % base;
+        num /= base;
+    }
+    for (int i = size - 1; i >= 0; i--)
+    {
+        cout << possibleCharacters[number[i]];
+    }
+    cout << endl;
+}
+
+void ConvertToDecimal(int num, int base)
+{
+    int total = 0;
+    if (base < 2 || base > 36) 
+    {
+        cout << "Base too low or too high! Base set to 10";
+        base = 10;
+    }
+    int index = 0;
+    while(num > 0)
+    {
+        total +=(num % 10) * Pow(base, index);
+        num /= 10; 
+        index++;
+    }
+    cout << total << endl;
+
+}
+
+int main(){
+    int num;
+    int base;
+    cout << "Please enter num : ";
+    cin >> num;
+    cout << "Please enter base : ";
+    cin >> base;
+    ConvertToDecimal(num, base);
+    
+    
+    return 0;
+}
