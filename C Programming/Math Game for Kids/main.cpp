fadc4268751b180868d97ed8ee8309f7c6c250c9
NanoCode012 <kevinvong@rocketmail.com>
2018-06-29 23:06:24 +0700

    Updated MathGameForKids to use a lookup table and removed modulo

--- C Programming/Math Game for Kids/main.cpp
+++ C Programming/Math Game for Kids/main.cpp
@@ -6,39 +6,34 @@ using namespace std;
 
 int main(){
     srand(time(NULL));
+
     const int limit = 15;
+    const char* signs = "+-*/";
+
     int count = 0;
     for (int i = 0; i < 10; i++){
         int a = rand() % limit;
         int b = rand() % limit;
-        int signInt = rand() % 5;
-        char sign;
+        
+        int signInt = rand() % 4;
         int val;
         int userInput;
         
         switch(signInt){
             case 0: 
-                sign = '+';
                 val = a + b;
                 break;
             case 1:
-                sign = '-';
                 val = a - b;
                 break;
             case 2:
-                sign = '*';
                 val = a * b;
                 break;
             case 3:
-                sign = '/';
                 val = a / b;
                 break;
-            case 4:
-                sign = '%';
-                val = a % b;
-                break;
         }
-        cout << a << " " << sign << " " << b << " = ";
+        cout << a << " " << signs[signInt] << " " << b << " = ";
         
         cin >> userInput;
         if (userInput == val) count++;
