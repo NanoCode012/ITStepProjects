2c2970adbd9471cfb73f71ef6c5a90ef11c3f2c6
NanoCode012 <kevinvong@rocketmail.com>
2018-07-12 13:31:44 +0700

    Added Calculator WinAPI

---
+++ C++ Programming WinAPI/Calculator/Calculator/Fraction.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+
+class Fraction
+{
+private:
+    int numerator;
+    int denominator;
+public:
+    Fraction(int num = 0, int denom = 1);
+    Fraction(const Fraction &other);
+
+    Fraction operator + (const Fraction &other);
+    Fraction operator - (const Fraction &other);
+    Fraction operator * (const Fraction &other);
+    Fraction operator / (const Fraction &other);
+    void operator = (const Fraction &other);
+
+    void SetInitialValue(int num, int denom);
+
+    Fraction Add(Fraction other);
+    Fraction Subtract(Fraction other);
+    Fraction Multiply(Fraction other);
+    Fraction Divide(Fraction other);
+
+    Fraction Simplify(int num, int denom);
+
+    int Compare(int num);
+
+    void Show();
+	
+	float GetValue();
+};
+
+int GCD(int a, int b);
